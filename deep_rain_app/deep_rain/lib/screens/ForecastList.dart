11321a94cf5718a8b3957aed627c48a1c1092cd0
tillreitlinger <Till-Reitlinger@outlook.de>
2020-04-27 13:39:04 +0200

    Fixed some UI bugs.

--- deep_rain_app/deep_rain/lib/screens/ForecastList.dart
+++ deep_rain_app/deep_rain/lib/screens/ForecastList.dart
@@ -1,4 +1,5 @@
 import 'package:deep_rain/DataObjects/ForecastListItem.dart';
+import 'package:deep_rain/global/GlobalValues.dart';
 import 'package:deep_rain/global/UIText.dart';
 import 'package:deep_rain/services/Database.dart';
 import 'package:flutter/material.dart';
@@ -9,6 +10,7 @@ import 'package:deep_rain/Widgets/forecast_list_widget.dart';
 This screen displays the forecast data as list. Cause of StreamProvider, always the latest data will be displayed.
  */
 class ForecastList extends StatelessWidget {
+  GlobalValues _globalValues = GlobalValues();
   @override
   Widget build(BuildContext context) {
     UIText _uiText = UIText();
@@ -17,7 +19,7 @@ class ForecastList extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.brown[50],
         appBar: AppBar(
-          title: Text(_uiText.forecastListAppTitle),
+          title: Text(_uiText.forecastListAppTitle + " " + _globalValues.getAppRegionCity()),
           elevation: 0.0,
         ),
         body: ForecastListWidget()
