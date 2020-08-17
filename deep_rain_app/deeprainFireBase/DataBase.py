335c989d41b915420d96276b5925686229731982
tillreitlinger <Till-Reitlinger@outlook.de>
2020-08-17 09:59:54 +0200

    The right time is shown in the slider label and the forecast list.

--- deep_rain_app/deeprainFireBase/DataBase.py
+++ deep_rain_app/deeprainFireBase/DataBase.py
@@ -201,5 +201,15 @@ if __name__ == '__main__':
     upload_data_to_firbase(forecast_images, time_steps_of_images)
 
 
-
+    abc = ['a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't']
+    def upload_time_steps(time_steps):
+        time_collection = db.collection('TimeSteps')
+        id = 0
+        for time in time_steps:
+            doc_ref = time_collection.document(abc[id])
+            doc_ref.set({
+                'time' : time
+            })
+            id = id + 1
+    upload_time_steps(time_steps_of_images)
 
